a2205faa1fac6cdc179af83369ab4956ae67eec3
Angela J. Holden <angelajholden@gmail.com>
2020-05-06 19:56:28 -0500

    adding the pie chart for macros

--- nutrition/index.php
+++ nutrition/index.php
@@ -7,12 +7,13 @@
 
 			<div class="block__title">
 				<h2 class="block__subheading">Nutrition</h2>
+				<!-- <h3 class="block__subsubhead">Macros</h3> -->
 			</div>
 
-				<?php include_once('../partials/header.php'); ?>
+				<?php //include_once('../partials/header.php'); ?>
 
         <main class="main" role="main">
-
+        	<img class="pie_chart" src="../img/piechart.png" alt="Macros Piechart">
         </main>
 
         <?php include_once('../partials/footer.php'); ?>
